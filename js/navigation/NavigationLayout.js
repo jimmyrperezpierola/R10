8c22661639b1c5e1241f099f11e2bb29da241959
im-chang <mchang0718@gmail.com>
2018-08-21 13:19:03 -0700

    add navigation icons

--- js/navigation/NavigationLayout.js
+++ js/navigation/NavigationLayout.js
@@ -8,30 +8,60 @@ import About from "../screens/About";
 import Map from "../screens/Map";
 import Favs from "../screens/Favs";
 import Schedule from "../screens/Schedule";
+import Icon from "react-native-vector-icons/Ionicons";
+import { sharedNavigationOptions } from "./config";
 
-const mapStack = createStackNavigator({
-  Map: {
-    screen: Map
+const mapStack = createStackNavigator(
+  {
+    Map: {
+      screen: Map
+    }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
   }
-});
+);
 
-const aboutStack = createStackNavigator({
-  About: {
-    screen: About
+const aboutStack = createStackNavigator(
+  {
+    About: {
+      screen: About
+    }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
   }
-});
+);
 
-const favsStack = createStackNavigator({
-  Favs: {
-    screen: Favs
+const favsStack = createStackNavigator(
+  {
+    Favs: {
+      screen: Favs
+    }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
   }
-});
+);
 
-const scheduleStack = createStackNavigator({
-  Schedule: {
-    screen: Schedule
+const scheduleStack = createStackNavigator(
+  {
+    Schedule: {
+      screen: Schedule
+    }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      ...sharedNavigationOptions(navigation)
+    })
   }
-});
+);
 
 export default createBottomTabNavigator(
   {
@@ -41,10 +71,31 @@ export default createBottomTabNavigator(
     Schedule: scheduleStack
   },
   {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === "About") {
+          iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+        } else if (routeName === "Map") {
+          iconName = `ios-map`;
+        } else if (routeName === "Favs") {
+          iconName = `ios-heart${focused ? "" : "-empty"}`;
+        } else if (routeName === "Schedule") {
+          iconName = `ios-calendar`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return <Icon name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+
     tabBarOptions: {
       activeTintColor: "#e91e63",
       labelStyle: {
-        fontSize: 12
+        fontFamily: "Montserrat",
+        fontSize: 10
       },
       style: {
         backgroundColor: "beige"
