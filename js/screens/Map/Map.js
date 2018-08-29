eda90414b197bb404188318b5ff69e90a84b8c40
im-chang <mchang0718@gmail.com>
2018-08-29 10:57:24 -0700

    add mapview for maps

--- js/screens/Map/Map.js
+++ js/screens/Map/Map.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styles from "./styles";
+import { WebView } from "react-native";
 
 const Map = () => {
   return (
-    <View style={styles.container}>
-      <Text> Map Page </Text>
-    </View>
+    <WebView
+      source={{
+        uri:
+          "https://www.google.ca/maps/place/RED+Academy/@49.2633514,-123.1403218,17z/data=!3m1!4b1!4m5!3m4!1s0x548673c79e1ac4fb:0x7edde857c28340ba!8m2!3d49.2633479!4d-123.1381278"
+      }}
+      style={{ height: "100%", width: "100%" }}
+    />
   );
 };
 
